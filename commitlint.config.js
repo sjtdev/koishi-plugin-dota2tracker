20f1947de921ccb8f848b1ad6cba57e699c24359
sjtdev <87354389+sjtdev@users.noreply.github.com>
2025-10-14 12:33:26 +0800

    chore(ci): setup husky v9+ commit-msg hook

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+// commitlint.config.js
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2, // 2 表示 error 级别
+      'always',
+      [
+        // 在这里继承默认规则，并加入你自己的类型
+        'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test',
+        'refine' // <-- 在这里加入我们自定义的类型
+      ]
+    ]
+  }
+};
