d7e577c749a19768f8852cbe9eacee24ffa9f150
sjtdev <87354389+sjtdev@users.noreply.github.com>
2026-01-01 19:12:47 +0800

    improve(command/query-hero): 适配提示消息撤回；增加了查询失败的错误提示信息。

--- src/app/commands/query-hero.command.ts
+++ src/app/commands/query-hero.command.ts
@@ -1,26 +1,35 @@
 import { Context } from "koishi";
 import { TemplateType } from "../common/types";
 import { handleError } from "../common/error";
+import { TaskMessenger } from "./_helper";
 
 export function registerQueryHeroCommand(ctx: Context): void {
+  const name = "query-hero";
+  const logger = ctx.logger("command/" + name);
   ctx
-    .command("dota2tracker.query-hero <input_data>")
+    .command(`dota2tracker.${name} <input_data>`)
     .option("random", "-r")
     .alias("查询英雄")
     .action(async ({ session, options }, input_data) => {
-      const name = "query-hero";
+      const task = new TaskMessenger(session);
       try {
         if (input_data || options.random) {
-          await session.send(session.text(".querying_hero"));
+          await task.send(session.text(".querying_hero"));
           const languageTag = await ctx.dota2tracker.i18n.getLanguageTag({ session });
           const heroData = await ctx.dota2tracker.hero.getHeroDetails(input_data, languageTag, options.random);
-          if (!heroData) return session.text(".not_found");
+          if (!heroData) {
+            await task.finish();
+            return session.text(".not_found");
+          }
           const image = await ctx.dota2tracker.image.renderToImageByFile(heroData, ctx.config.template_hero, TemplateType.Hero, languageTag);
           const message = ctx.dota2tracker.messageBuilder.buildHeroMessage(heroData);
+          await task.finish();
           await session.send(message + image);
         }
       } catch (error) {
-        handleError(error, ctx.logger(name), ctx.dota2tracker.i18n, ctx.config);
+        await task.finish();
+        handleError(error, logger, ctx.dota2tracker.i18n, ctx.config);
+        return session.text(".query_failed");
       }
     });
 }
