476a7bc98fe275e3049e1f0f7a2efbee154e0699
sjtdev <87354389+sjtdev@users.noreply.github.com>
2026-01-01 19:19:33 +0800

    improve(command/query-player): 适配提示消息撤回；增加了查询失败时的错误提示信息。

--- src/app/commands/query-player.command.ts
+++ src/app/commands/query-player.command.ts
@@ -1,31 +1,36 @@
 import { Context } from "koishi";
 import { TemplateType } from "../common/types";
-import { resolvePlayerAndHandleErrors } from "./_helper";
+import { resolvePlayerAndHandleErrors, TaskMessenger } from "./_helper";
 import { handleError } from "../common/error";
 
 export function registerQueryPlayerCommand(ctx: Context) {
+  const name = "query-player";
+  const logger = ctx.logger("command/" + name);
   ctx
     .command("dota2tracker.query-player <input_data>")
     .option("hero", "-o <value:string>")
     .alias("查询玩家")
     .action(async ({ session, options }, input_data) => {
-      const name = "query-player";
+      const task = new TaskMessenger(session);
       try {
         if (session.guild || (!session.guild && input_data)) {
           const steamId = await resolvePlayerAndHandleErrors(ctx, session, input_data);
           if (steamId === null) return;
-          session.send(session.text(".querying_player"));
+          await task.send(session.text(".querying_player"));
           const heroId = ctx.dota2tracker.i18n.findHeroIdInLocale(options.hero);
           const languageTag = await ctx.dota2tracker.i18n.getLanguageTag({ session });
           const formattedPlayerData = await ctx.dota2tracker.player.getFormattedPlayerData(steamId, heroId, languageTag);
           const image = await ctx.dota2tracker.image.renderToImageByFile(formattedPlayerData, ctx.config.template_player, TemplateType.Player, languageTag);
           const message = ctx.dota2tracker.messageBuilder.buildPlayerMessage(steamId);
+          await task.finish();
           return message + image;
         } else {
           return session.text("commands.dota2tracker.common.messages.user_not_in_group");
         }
       } catch (error) {
-        handleError(error, ctx.logger(name), ctx.dota2tracker.i18n, ctx.config);
+        await task.finish();
+        handleError(error, logger, ctx.dota2tracker.i18n, ctx.config);
+        return session.text(".query_failed");
       }
     });
 }
